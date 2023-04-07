17161b3a0c2db628f0e9c58d882764d5481fcc2d
vikmaamaa <maamaavictor@yahoo.com>
2023-04-07 21:08:07 +0100

    feat: protecting routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login() {
-        return "This is my login method";
+    public function login(LoginUserRequest $request) {
+        $request->validated($request->all());
+
+
+        if(!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('', 'Credentials do not match', 401);
+        }
+
+        $user  = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of ' . $user->name)->plainTextToken
+        ]);
+
     }
 
 
